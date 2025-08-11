335e3a4bd9bd6d276b78de9c896ee6f58229762d
NABIL HARIZ <hariznabil663@gmail.com>
2025-08-11 16:27:26 +0200

    add animation aos

--- src/componants/HeroSection.jsx
+++ src/componants/HeroSection.jsx
@@ -1,37 +1,51 @@
-import React from 'react'
-import '../styles/HeroSection.css'
-import img from '../assets/sec-img.jpg'
-
-
+import React, { useEffect } from 'react';
+import '../styles/HeroSection.css';
+import img from '../assets/sec-img.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const HeroSection = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); 
+  }, []);
+
   return (
-     <section className='hero-section'>
-       <div className="section-content">
-        <div className='stars'>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-
-        </div>
-        <div className='content-section'>
-            <div className='content-left'>
-              <h3>Explorez notre collection de films</h3>
-            </div>
-            <div className="content-right">
-                <img src={img} alt=""/>
-                <h3>+ 50 films & séries</h3>
-                <h4>Ausez-vous bien ! </h4>
-
-            </div>
-
-        </div>
+    <section className='hero-section'>
+      <div className="section-content" data-aos="fade-right" data-aos-delay="500" >
+        
        
-       </div>
-     </section>
-  )
+        <div className='stars'>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+        </div>
+
+    
+        <div className='content-section'>
+          
+          <div 
+            className='content-left' 
+          >
+            <h3>Explorez notre collection de films</h3>
+          </div>
+
+        
+          <div 
+            className="content-right"  
+            data-aos="fade-left"
+            data-aos-delay="1000" 
+          >
+            <img src={img} alt=""/>
+            <h3>+ 50 films & séries</h3>
+            <h4>Ausez-vous bien ! </h4>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
