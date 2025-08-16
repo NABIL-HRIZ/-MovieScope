b92f8c1fd874cc3ae5c491963b528eadfd2f0fef
NABIL HARIZ <hariznabil663@gmail.com>
2025-08-16 02:42:08 +0200

    compression images

--- src/componants/GenresIcon.js
+++ src/componants/GenresIcon.js
@@ -3,14 +3,14 @@
 const GenresBackground={
     Action:"./action.jpg",
     SciFi:"./science.jpg",
-    Thriller:'/Thriller.jpg',
+    Thriller:'./thrilly.jpg',
     Drama:"./drama.jpg",
     Animation:'./animation.jpg',
     Romance:"./romance.jpg",
     Crime:"./crime.jpg",
     Adventure:"./adventure.jpg",
-    Mystery:'./Mystery.jpg',
-    Comedy:'./comedie.jpg',
+    Mystery:'./mystery.jpg',
+    Comedy:'./comedy.jpg',
     War:'./war.jpg',
     Musical:'./musical.jpg',
     Biography:'./biography.jpg'
